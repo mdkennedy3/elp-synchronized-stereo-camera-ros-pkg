b7c98b7629e30de6290bc83a1809b8407acf8873
Monroe Kennedy <monroe.kennedy3@gmail.com>
2018-07-25 17:07:50 -0400

    updated udev rules creator

--- scripts/create_udev_rules.sh
+++ scripts/create_udev_rules.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo ""
-echo "This script copies a udev rule to /etc to facilitate bringing"
-echo "up the astra usb connection as /dev/astra*"
+echo "This script copies the udev rule to /etc to facilitate bringing"
+echo "up the elp stereo usb connection as /dev/elp_sync*"
 echo ""
 
 sudo cp `rospack find elp-stereo-synchronized-ros-pkg`/99-elp-stereo-sync-camera.rules /etc/udev/rules.d
